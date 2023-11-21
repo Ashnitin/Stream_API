346dac85d9daa208525743123371f8d8e555c95f
unknown <ashwinijadhav129aj@gmail.com>
2023-11-21 22:01:41 +0530

    Added:Ability to transform each element to double use Map

--- src/com/bridglabz/StreamOperation.java
+++ src/com/bridglabz/StreamOperation.java
@@ -2,6 +2,7 @@ package com.bridglabz;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamOperation {
     public static void main(String[] args) {
@@ -10,5 +11,14 @@ public class StreamOperation {
         l.add("Rucha");
         l.add("Pooja");
         l.stream().forEach(System.out::println);
+
+        List<Integer> l1=new ArrayList<Integer>();
+        l1.add(10);
+        l1.add(20);
+        l1.add(30);
+        l1.add(40);
+        System.out.println(l1);
+        List<Integer> l2=l1.stream().map(i->i*2).collect(Collectors.toList());
+        System.out.println(l2);
     }
 }
