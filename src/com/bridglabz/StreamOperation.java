46fb94f8c264fa37159d5ce3171f5ee96470f4db
unknown <ashwinijadhav129aj@gmail.com>
2023-11-21 12:12:40 +0530

    [Added]:Stream and iterate to show using foreach

--- src/com/bridglabz/StreamOperation.java
+++ src/com/bridglabz/StreamOperation.java
@@ -1,7 +1,14 @@
 package com.bridglabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StreamOperation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Stream API");
+        List <String> l= new ArrayList<String>();
+        l.add("Ashwini");
+        l.add("Rucha");
+        l.add("Pooja");
+        l.stream().forEach(System.out::println);
     }
 }
